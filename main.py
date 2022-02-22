838d3a8a91e759797b308872532f24fccd820ad8
Miles Strait <milesstrait@gmail.com>
2022-02-22 12:57:45 -0800

    2-22-22

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 font_pixeltype = pygame.font.Font('font/Pixeltype.ttf', 50)
+
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
-score_surface = font_pixeltype.render('Score', True, 'Black')
-font_rect = score_surface.get_rect(midtop = (400,50))
+
+score_surface = font_pixeltype.render('Score', True, (64,64,64))
+score_rect = score_surface.get_rect(center = (400,50))
+
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
@@ -21,13 +24,29 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        
+        if event.type == pygame.MOUSEMOTION:
+            if player_rect.collidepoint(event.pos):
+                exit()
+            
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
-    screen.blit(score_surface, font_rect)
+    pygame.draw.rect(screen, (192,232,236), score_rect)
+    pygame.draw.rect(screen, (192,232,236), score_rect, 10)
+ 
+    screen.blit(score_surface, score_rect)
     screen.blit(snail_surface,snail_rect)
     snail_rect.x -= 4
     if snail_rect.right <= 0: snail_rect.left = 800
     screen.blit(player_surface,player_rect)
+
+    keys = pygame.key.get_pressed
+    keys[pygame.K_SPACE]
+    # if player_rect.colliderect(snail_rect):
+    #     exit()
+
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos):
+    #     print(pygame.mouse.get_pressed())
+
     pygame.display.update()
     clock.tick(60)
